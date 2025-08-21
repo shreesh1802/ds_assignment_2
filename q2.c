900e801231132af2958ab38174ac91c87cea0dad
shreesh <sgupta182005@gmail.com>
2025-08-21 12:10:20 +0530

    Add files via upload

---
+++ q2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int Bubblesort(int arr[], int n) {
+    int i, j, temp;
+    int swapped;
+
+    for (i = 0; i < n - 1; i++) {
+        swapped = 0; 
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = 1;
+            }
+        }
+        if (swapped == 0) {
+            break;
+        }
+    }
+    return 0;
+}   
