88e7aab0ff7fa123bb2b85b93b704610ee803adb
shreesh <sgupta182005@gmail.com>
2025-08-21 12:19:04 +0530

    Add files via upload

---
+++ q4_b.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int reversestring(string str) {
+    int n = str.length();
+    for (int i = 0; i < n / 2; i++) {
+        swap(str[i], str[n - i - 1]);
+    }
+    cout << "Reversed string: " << str << endl;
+    return 0;
+}
