24f3d865a898fed52a54ea0ede3f3971d84e276f
shreesh <sgupta182005@gmail.com>
2025-08-21 12:17:11 +0530

    Add files via upload

---
+++ q4_a.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int stringconcatenate(const char *str1 , const char *str2, char *result) {
+
+    int length = sizeof(str1)
+    for(int i = 0; i < sizeof(str2); i++) {
+        str1[length++] = str2[i];
+    }
+    
