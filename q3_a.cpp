2cf9c0c4656886740e2f850b01e19e4715dd7223
shreesh <sgupta182005@gmail.com>
2025-08-21 12:10:42 +0530

    Add files via upload

---
+++ q3_a.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int findMissingLinear(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] != i + 1)
+            return i + 1;
+    }
+    return n;
+}
+
+int main() {
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+    int arr[n - 1];
+    cout << "Enter sorted array of size " << n - 1 << ":\n";
+    for (int i = 0; i < n - 1; i++)
+        cin >> arr[i];
+
+    cout << "Missing number (Linear method): " << findMissingLinear(arr, n) << endl;
+    return 0;
+}
+
