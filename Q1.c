f80ce0d06226d147ea3c87f0a1f99a110aeeade8
shreesh <sgupta182005@gmail.com>
2025-08-21 12:10:01 +0530

    Add files via upload

---
+++ Q1.c
@@ -0,0 +1,21 @@
+//  Q1
+
+#include <stdio.h>
+
+int BinarySearch(int arr[], int size, int target) {
+    int left = 0;
+    int right = size - 1;
+
+    while (left <= right) {
+        int mid = (right + left) / 2;
+
+        if (arr[mid] == target) {
+            return mid; 
+        } else if (arr[mid] < target) {
+            left = mid + 1; 
+        } else {
+            right = mid - 1; 
+        }
+    }
+    return -1;  
+}
