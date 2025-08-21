99fd9058bdb9922d517380c3e6b056fac7f12ab4
shreesh <sgupta182005@gmail.com>
2025-08-21 12:11:09 +0530

    Add files via upload

---
+++ q3_b.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int findMissingBinary(int arr[], int n) {
+    int left = 0, right = n - 2; 
+    while (left <= right) {
+        int mid = (left + right) / 2;
+        if (arr[mid] == mid + 1) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return left + 1;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 6};
+    int n = 6;
+    cout << "Missing number (Binary Search): " << findMissingBinary(arr, n) << endl;
+    return 0;
+}
+
+
